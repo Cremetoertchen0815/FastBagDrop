aa06626f449c7a5857599e4d36141957b53a7c56
Creme <Mircosoft_heck@web.de>
2022-10-18 18:21:38 +0200

    Got started with writing the main class

---
+++ src/main/java/dhbw/group2/FBDMachine.java
@@ -0,0 +1,48 @@
+package dhbw.group2;
+
+public class FBDMachine {
+    private String serialNumber;
+    private String manufacturer;
+    private StateEnum state;
+
+    public void startup() {
+
+    }
+
+    public void shutdown() {
+
+    }
+
+    public void unlock() {
+
+    }
+
+    public CSVItem importFromCSV() {
+        return null;
+    }
+
+    public void checkIn() {
+
+    }
+
+    public void analyseData() {
+
+    }
+
+    public void export() {
+
+    }
+
+    public float determineWeight() {
+        return 0;
+    }
+
+    public void scanBaggageTag() {
+
+    }
+
+    public void investigateExplosives(FederalPoliceOfficer responsibleOfficer) {
+
+    }
+
+}
