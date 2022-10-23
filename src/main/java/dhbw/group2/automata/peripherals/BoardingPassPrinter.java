c52f8d82d280198dd89fb0ef70e0f792a6c659f1
Creme <Mircosoft_heck@web.de>
2022-10-23 11:31:50 +0200

    Implemented future task #3

--- src/main/java/dhbw/group2/automata/peripherals/BoardingPassPrinter.java
+++ src/main/java/dhbw/group2/automata/peripherals/BoardingPassPrinter.java
@@ -3,12 +3,16 @@ package dhbw.group2.automata.peripherals;
 import dhbw.group2.plane.PlaneSeat;
 import dhbw.group2.plane.boarding.BaggageTag;
 import dhbw.group2.plane.boarding.BoardingPass;
+import dhbw.group2.plane.boarding.BoardingQRCode;
 
 import java.util.List;
 
 public class BoardingPassPrinter implements IPrinter<BoardingPass> {
 
 
+    private String source;
+    private String destination;
+    private String flight;
     private List<BaggageTag> baggageTags;
     private PlaneSeat planeSeat;
 
@@ -20,8 +24,20 @@ public class BoardingPassPrinter implements IPrinter<BoardingPass> {
         this.planeSeat = planeSeat;
     }
 
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setFlight(String flight) {
+        this.flight = flight;
+    }
+
     @Override
     public BoardingPass print() {
-        return new BoardingPass(baggageTags, planeSeat);
+        return new BoardingPass(baggageTags, new BoardingQRCode(source, destination, flight), planeSeat);
     }
 }
---
+++ src/main/java/dhbw/group2/plane/boarding/BoardingQRCode.java
@@ -0,0 +1,7 @@
+package dhbw.group2.plane.boarding;
+
+public class BoardingQRCode {
+
+    //TODO: Implement boarding QR code
+    public BoardingQRCode(String source, String destination, String flight) { }
+}
