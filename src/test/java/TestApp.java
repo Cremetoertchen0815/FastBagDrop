128af88d2931b789fb12d47ef20467e669175ae2
kenlenden <108568697+kenlenden@users.noreply.github.com>
2022-10-25 11:30:50 +0200

    change test method

--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -1,12 +1,22 @@
 import dhbw.group2.Main;
 import dhbw.group2.automata.FBDMachine;
+import dhbw.group2.automata.StateEnum;
+import dhbw.group2.humans.FederalPolice;
+import dhbw.group2.humans.FederalPoliceOfficer;
 import dhbw.group2.humans.Passenger;
 import dhbw.group2.humans.ServiceAgent;
 import dhbw.group2.plane.boarding.Baggage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import javax.swing.plaf.metal.MetalBorders;
+
+import java.beans.FeatureDescriptor;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 public class TestApp {
@@ -17,11 +27,6 @@ public class TestApp {
     public void setUp() {
         agent = new ServiceAgent();
         auto = new FBDMachine();
-
-        auto.importFromCSV(agent);
-        auto.warmSimulation();
-        auto.checkIn();
-        auto.export();
     }
 
     @Test
